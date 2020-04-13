055a6773fc954517a732b71cb755edc02b753bad
SaadBadr <saadbadr23@gmail.com>
2020-04-13 23:50:44 +0200

    separating probability array to be loaded and saved to a file

--- main.py
+++ main.py
@@ -3,21 +3,27 @@ import utility
 
 def main():
 
-    # ENCODING
+    ########################################### ENCODING ##########################################################
     block_size = 4
     imagePath = "test2.png"
     # name of the encoded file that will be created
     encodedFile = "encoded.npy"
+    # name of the file to save the probabilty 1D numpy array in it
+    probabiltyFile = "probability.npy"
     float_type = 'float64' #you can choose numpy float types as float64(default), float32, float16, and longdouble
-    probability = utility.encode(block_size, imagePath, encodedFile, float_type)
+    utility.encode(block_size, imagePath, encodedFile,probabiltyFile, float_type)
 
-    # DECODING
+    ########################################### DECODING ##########################################################
     # rows = 183
     # columns = 275
     rows = 256
     columns = 256
     resultImage = "result.png"
-    utility.decode(block_size, rows, columns, probability, encodedFile, resultImage)
+    # name of the encoded file to be decoded
+    encodedFile = "encoded.npy"
+    # name of the file having the probabilty 1D numpy array
+    probabiltyFile = "probability.npy"
+    utility.decode(block_size, rows, columns, probabiltyFile, encodedFile, resultImage)
 
 
 main()
--- utility.py
+++ utility.py
@@ -5,12 +5,12 @@ import cv2
 import numpy
 
 
-def encode(block_size ,imagePath="test.jpg",encodedFile="encoded", float_type='float64'):
+def encode(block_size ,imagePath="test.jpg",encodedFile="encoded",probabilityFile="probability.npy", float_type='float64'):
     img = cv2.imread(imagePath, cv2.IMREAD_GRAYSCALE).flatten()
     img = numpy.append(img, [0] * (block_size - len(img) % block_size))
     codes = numpy.array([])
     probability = (collections.Counter(img))
-
+    print("Encoding Started")
     # for i in range(0, len(probability)):
         # probability[i] /= len(img)
 
@@ -29,20 +29,26 @@ def encode(block_size ,imagePath="test.jpg",encodedFile="encoded", float_type='f
             print("encoding error")
         codes = numpy.append(codes, [x])
     numpy.save('original.npy', img)
+    print("Encoding Done:")
     numpy.save(encodedFile, codes) # save
-    print("Encoding Done")
+    print(" ->" + encodedFile + " is created")
+    numpy.save(probabilityFile, prob) # save
+    print(" ->" + probabilityFile + " is created\n")
+
     return prob
 
 
 def decode(block_size, rows, columns, probability, encodedFile="encoded", resultImage="result.jpg"):
     codes = numpy.load(encodedFile)
+    probability = numpy.load(probability)
     img = numpy.array([])
+    print("Decoding Started")
     for i in range(0, len(codes)):
         x = d.decoding(probability, codes[i], block_size)
         img = numpy.append(img, x)
     img = img[:(len(img) - block_size + (rows * columns) % block_size)]
-    print("Decoding Done")
+    print("Decoding Done:")
     img = img.reshape(rows, columns)
     cv2.imwrite(resultImage, img)
-    print(resultImage + ' is created')
+    print(" ->" + resultImage + ' is created\n')
 
